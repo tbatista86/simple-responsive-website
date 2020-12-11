15b6a8ce664f6e51c15f828c4ff9b3228085eda1
Tiago Batista <tiagobatista86@gmail.com>
2020-12-11 11:29:18 -0300

    create herosction and info section

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { ArrowForward, ArrowRight, HeroBg, HeroBtnWrapper, HeroContainer, HeroContent, HeroH1, HeroP, VideoBg } from './HeroElements'
+import { Button } from '../ButtonElement'
+import Video from '../../videos/video.mp4'
+
+const HeroSection = () => {
+  const [hover, setHover] = useState(false)
+
+  const onHover = () => {
+    setHover(!hover)
+  }
+
+  return (
+    <HeroContainer id="home">
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Virtual Banking Made Easy</HeroH1>
+        <HeroP>
+          Sign up for a new account today and recive #250 in credit towards your next payment.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button 
+            to="signup" 
+            onMouseEnter={onHover} 
+            onMouseLeave={onHover} 
+            primary="true" 
+            dark="true"
+          >
+            Get started {hover ? <ArrowForward /> : <ArrowRight/>}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  )
+}
+
+export default HeroSection
+
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { FaBars } from 'react-icons/fa'
 import { MobileIcon, Nav, NavbarContainer, NavItem, NavLogo, NavMenu, NavLinks, NavBtn, NavBtnLink } from './NavbarElements'
 
-const Navbar = () => {
+const Navbar = ({ toggle }) => {
   return (
     <>
       <Nav>
         <NavbarContainer>
-          <NavLogo to="/">dolla </NavLogo>
-          <MobileIcon>
+          <NavLogo to="/">dolla</NavLogo>
+          <MobileIcon onClick={toggle}>
             <FaBars />
           </MobileIcon>
           <NavMenu>
