7805f90dc62ef0108dcd30cf32243118fdf9d65b
Tiago Batista <tiagobatista86@gmail.com>
2020-12-11 16:38:00 -0300

    create footer section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import './App.css'
 import Home from './pages';
+import Signin from './pages/signin';
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signin" exact component={Signin} />
+      </Switch>
     </Router>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { FooterContainer, FooterLink, FooterLinkItems, FooterLinksContainer, FooterLinksWrapper, FooterLinkTitle, FooterWrap } from './FooterElements'
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>              
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to="/signin">How it works</FooterLink>
+              <FooterLink to="/signin">Testimonials</FooterLink>
+              <FooterLink to="/signin">Carres</FooterLink>
+              <FooterLink to="/signin">Investos</FooterLink>
+              <FooterLink to="/signin">Terms of Service</FooterLink>
+            </FooterLinkItems>
+
+            <FooterLinkItems>              
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to="/signin">How it works</FooterLink>
+              <FooterLink to="/signin">Testimonials</FooterLink>
+              <FooterLink to="/signin">Carres</FooterLink>
+              <FooterLink to="/signin">Investos</FooterLink>
+              <FooterLink to="/signin">Terms of Service</FooterLink>
+            </FooterLinkItems>
+
+            <FooterLinkItems>              
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to="/signin">How it works</FooterLink>
+              <FooterLink to="/signin">Testimonials</FooterLink>
+              <FooterLink to="/signin">Carres</FooterLink>
+              <FooterLink to="/signin">Investos</FooterLink>
+              <FooterLink to="/signin">Terms of Service</FooterLink>
+            </FooterLinkItems>
+
+            <FooterLinkItems>              
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to="/signin">How it works</FooterLink>
+              <FooterLink to="/signin">Testimonials</FooterLink>
+              <FooterLink to="/signin">Carres</FooterLink>
+              <FooterLink to="/signin">Investos</FooterLink>
+              <FooterLink to="/signin">Terms of Service</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+      </FooterWrap>
+    </FooterContainer>
+  )
+}
+
+export default Footer
